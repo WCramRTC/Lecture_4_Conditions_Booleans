1d7891afbee6257fb5f8b8f0ade0ad2f4d3926c0
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-10-03 18:52:40 -0700

    Update Program.cs

--- Lecture_4_Conditions_Booleans/Program.cs
+++ Lecture_4_Conditions_Booleans/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +15,29 @@ namespace Lecture_4_Conditions_Booleans
         {
 
 
+
         } // Main
+
+        public static void MathExample()
+        {
+            //Adding Slightly over 100:
+            //The restaurant has 175 normal chairs and 20 chairs for babies.How many chairs does the restaurant have in total ?
+            Console.Write("How many chairs are there: ");
+            // Number of normal chairs
+            string strOfChairs = Console.ReadLine();
+            int numOfChairs = int.Parse(strOfChairs);
+
+
+            // Number of chairs for babys
+            Console.Write("\nEnter the number of baby chairs: ");
+            string strBabyChair = Console.ReadLine();
+            int babyChair = int.Parse(strBabyChair);
+
+            // How many total chairs
+            int sum = numOfChairs + babyChair;
+
+            Console.WriteLine("The number of chairs is " + sum);
+        }
 
     } // class
 } // namespace
